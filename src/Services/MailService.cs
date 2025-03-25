2cd0eaa2acf0f01b86b459f4348a2725a68ed114
Руденко Александра <kemmed28@mail.ru>
2025-03-25 14:23:55 +0300

    Export mail settings to json file

--- src/Services/MailService.cs
+++ src/Services/MailService.cs
@@ -1,15 +1,25 @@
 ﻿using System.Net.Mail;
 using System.Net;
+using System.Text.Json;
 
 namespace diplom.Services
 {
     public class MailService
     {
+        private readonly MailSettings _mailSettings;
+        public MailService()
+        {
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "mailSettings.json");
+            string jsonString = File.ReadAllText(jsonFilePath);
+            var mailConfig = JsonSerializer.Deserialize<MailConfig>(jsonString);
+
+            //_mailSettings = mailConfig?.MailSettings ?? throw new InvalidOperationException("Ошибка при чтении параметров почты.");
+        }
         public void SendEmail(string body, string topic, string recipientEmail)
         {
 
             MailMessage mail = new MailMessage();
-            mail.From = new MailAddress("task.loom@mail.ru");
+            mail.From = new MailAddress(_mailSettings.FromEmail); 
             mail.To.Add(recipientEmail);
             mail.Subject = topic;
             mail.Body = body;
@@ -17,7 +27,7 @@ namespace diplom.Services
 
 
             SmtpClient smtpClient = new SmtpClient("smtp.mail.ru", 25);
-            smtpClient.Credentials = new NetworkCredential("task.loom@mail.ru", "hd4r9uPHfBAyqkUc0wrJ");
+            smtpClient.Credentials = new NetworkCredential(_mailSettings.FromEmail, _mailSettings.Password);
             smtpClient.EnableSsl = true;
 
 
---
+++ src/Services/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace diplom.Services
+{
+    public class MailSettings
+    {
+        public string FromEmail { get; set; }
+        public string Password { get; set; }
+    }
+    public class MailConfig
+    {
+        public MailSettings MailSettings { get; set; }
+    }
+}
