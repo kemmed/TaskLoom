c746c5ba7b0b102b83c123a2fae560186bb58b8d
Руденко Александра <kemmed28@mail.ru>
2025-03-25 20:03:48 +0300

    add main window with all projects

--- src/Models/Project.cs
+++ src/Models/Project.cs
@@ -1,8 +1,10 @@
 ﻿namespace diplom.Models
 {
     public enum ProjectStatus {InProcess, Completed, Frozen}
+    
     public class Project
     {
+
         public int ID { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
@@ -20,5 +22,22 @@
         public List<StatusType> StatusTypes { get; set; }
         public List<UserProject> UserProjects { get; set; }
         public List<Log> Logs { get; set; }
+
+        public string ConvertStatus(ProjectStatus projectStatus)
+        {
+            switch (projectStatus)
+            {
+                case ProjectStatus.Completed:
+                    return "Выполнен";
+                case ProjectStatus.InProcess:
+                    return "В процессе";
+                case ProjectStatus.Frozen:
+                    return "Отложен";
+                default:
+                    return "";
+
+            }
+        }
     }
+    
 }
--- src/Services/MailService.cs
+++ src/Services/MailService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Mail;
 using System.Net;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace diplom.Services
 {
@@ -9,7 +10,7 @@ namespace diplom.Services
         private readonly MailSettings _mailSettings;
         public MailService()
         {
-            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "mailSettings.json");
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Configuration/mailSettings.json");
             string jsonString = File.ReadAllText(jsonFilePath);
             var mailConfig = JsonSerializer.Deserialize<MailConfig>(jsonString);
 
@@ -19,7 +20,8 @@ namespace diplom.Services
         {
 
             MailMessage mail = new MailMessage();
-            mail.From = new MailAddress(_mailSettings.FromEmail); 
+            //mail.From = new MailAddress(_mailSettings.FromEmail); 
+            mail.From = new MailAddress("task.loom@mail.ru");
             mail.To.Add(recipientEmail);
             mail.Subject = topic;
             mail.Body = body;
@@ -27,7 +29,8 @@ namespace diplom.Services
 
 
             SmtpClient smtpClient = new SmtpClient("smtp.mail.ru", 25);
-            smtpClient.Credentials = new NetworkCredential(_mailSettings.FromEmail, _mailSettings.Password);
+            //smtpClient.Credentials = new NetworkCredential(_mailSettings.FromEmail, _mailSettings.Password);
+            smtpClient.Credentials = new NetworkCredential("task.loom@mail.ru", "hd4r9uPHfBAyqkUc0wrJ");
             smtpClient.EnableSsl = true;
 
 
